d0f2267b0c2641f2bcd7676a60c414d40f9b034c
samuelscheit <github@samuelscheit.com>
2026-05-10 02:11:08 +0200

    Implement scheduler postTask entry

--- packages/scheduler/cjs/scheduler-unstable_post_task.development.js
+++ packages/scheduler/cjs/scheduler-unstable_post_task.development.js
@@ -1,87 +1,150 @@
-'use strict';
+/**
+ * @license React
+ * scheduler-unstable_post_task.development.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
-const compatibilityTarget = 'scheduler@0.27.0';
-const entrypoint = 'scheduler/unstable_post_task';
-const unimplementedCode = 'FAST_REACT_UNIMPLEMENTED';
-
-function createUnsupportedError(exportName) {
-  const error = new Error(
-    `[fast-react] ${entrypoint}.${exportName} was called, but this ` +
-      `placeholder has no postTask scheduler behavior implementation yet. It ` +
-      `exists to track the accepted ${compatibilityTarget} package surface; ` +
-      `do not treat it as scheduler-compatible behavior.`
-  );
-
-  error.name = 'FastReactSchedulerUnimplementedError';
-  error.code = unimplementedCode;
-  error.entrypoint = entrypoint;
-  error.exportName = exportName;
-  error.compatibilityTarget = compatibilityTarget;
-
-  return error;
-}
-
-function createUnsupportedFunction(exportName) {
-  const fn = function fastReactSchedulerPostTaskUnimplementedPlaceholder() {
-    throw createUnsupportedError(exportName);
-  };
-
-  Object.defineProperty(fn, 'name', {
-    configurable: true,
-    value: exportName
-  });
-
-  return fn;
-}
-
-function definePlaceholderMetadata(exportsObject) {
-  Object.defineProperties(exportsObject, {
-    __FAST_REACT_PLACEHOLDER__: {
-      enumerable: false,
-      value: true
-    },
-    __FAST_REACT_ENTRYPOINT__: {
-      enumerable: false,
-      value: entrypoint
-    },
-    compatibilityTarget: {
-      enumerable: false,
-      value: compatibilityTarget
+"use strict";
+"production" !== process.env.NODE_ENV &&
+  (function () {
+    function runTask(priorityLevel, postTaskPriority, node, callback) {
+      deadline = getCurrentTime() + 5;
+      try {
+        currentPriorityLevel_DEPRECATED = priorityLevel;
+        var result = callback(!1);
+        if ("function" === typeof result) {
+          var continuationOptions = { signal: node._controller.signal },
+            nextTask = runTask.bind(
+              null,
+              priorityLevel,
+              postTaskPriority,
+              node,
+              result
+            );
+          void 0 !== scheduler.yield
+            ? scheduler
+                .yield(continuationOptions)
+                .then(nextTask)
+                .catch(handleAbortError)
+            : scheduler
+                .postTask(nextTask, continuationOptions)
+                .catch(handleAbortError);
+        }
+      } catch (error) {
+        setTimeout(function () {
+          throw error;
+        });
+      } finally {
+        currentPriorityLevel_DEPRECATED = 3;
+      }
     }
-  });
-}
-
-exports.unstable_scheduleCallback = createUnsupportedFunction(
-  'unstable_scheduleCallback'
-);
-exports.unstable_cancelCallback = createUnsupportedFunction(
-  'unstable_cancelCallback'
-);
-exports.unstable_shouldYield = createUnsupportedFunction(
-  'unstable_shouldYield'
-);
-exports.unstable_now = createUnsupportedFunction('unstable_now');
-exports.unstable_runWithPriority = createUnsupportedFunction(
-  'unstable_runWithPriority'
-);
-exports.unstable_next = createUnsupportedFunction('unstable_next');
-exports.unstable_wrapCallback = createUnsupportedFunction(
-  'unstable_wrapCallback'
-);
-exports.unstable_requestPaint = createUnsupportedFunction(
-  'unstable_requestPaint'
-);
-exports.unstable_forceFrameRate = createUnsupportedFunction(
-  'unstable_forceFrameRate'
-);
-exports.unstable_getCurrentPriorityLevel = createUnsupportedFunction(
-  'unstable_getCurrentPriorityLevel'
-);
-exports.unstable_Profiling = null;
-exports.unstable_ImmediatePriority = 1;
-exports.unstable_UserBlockingPriority = 2;
-exports.unstable_NormalPriority = 3;
-exports.unstable_LowPriority = 4;
-exports.unstable_IdlePriority = 5;
-
-definePlaceholderMetadata(module.exports);
+    function handleAbortError() {}
+    var perf = window.performance,
+      setTimeout = window.setTimeout,
+      scheduler = global.scheduler,
+      getCurrentTime = perf.now.bind(perf),
+      deadline = 0,
+      currentPriorityLevel_DEPRECATED = 3;
+    exports.unstable_IdlePriority = 5;
+    exports.unstable_ImmediatePriority = 1;
+    exports.unstable_LowPriority = 4;
+    exports.unstable_NormalPriority = 3;
+    exports.unstable_Profiling = null;
+    exports.unstable_UserBlockingPriority = 2;
+    exports.unstable_cancelCallback = function (node) {
+      node._controller.abort();
+    };
+    exports.unstable_forceFrameRate = function () {};
+    exports.unstable_getCurrentPriorityLevel = function () {
+      return currentPriorityLevel_DEPRECATED;
+    };
+    exports.unstable_next = function (callback) {
+      switch (currentPriorityLevel_DEPRECATED) {
+        case 1:
+        case 2:
+        case 3:
+          var priorityLevel = 3;
+          break;
+        default:
+          priorityLevel = currentPriorityLevel_DEPRECATED;
+      }
+      var previousPriorityLevel = currentPriorityLevel_DEPRECATED;
+      currentPriorityLevel_DEPRECATED = priorityLevel;
+      try {
+        return callback();
+      } finally {
+        currentPriorityLevel_DEPRECATED = previousPriorityLevel;
+      }
+    };
+    exports.unstable_now = getCurrentTime;
+    exports.unstable_requestPaint = function () {};
+    exports.unstable_runWithPriority = function (priorityLevel, callback) {
+      var previousPriorityLevel = currentPriorityLevel_DEPRECATED;
+      currentPriorityLevel_DEPRECATED = priorityLevel;
+      try {
+        return callback();
+      } finally {
+        currentPriorityLevel_DEPRECATED = previousPriorityLevel;
+      }
+    };
+    exports.unstable_scheduleCallback = function (
+      priorityLevel,
+      callback,
+      options
+    ) {
+      switch (priorityLevel) {
+        case 1:
+        case 2:
+          var postTaskPriority = "user-blocking";
+          break;
+        case 4:
+        case 3:
+          postTaskPriority = "user-visible";
+          break;
+        case 5:
+          postTaskPriority = "background";
+          break;
+        default:
+          postTaskPriority = "user-visible";
+      }
+      var controller = new TaskController({ priority: postTaskPriority });
+      options = {
+        delay:
+          "object" === typeof options && null !== options ? options.delay : 0,
+        signal: controller.signal
+      };
+      controller = { _controller: controller };
+      scheduler
+        .postTask(
+          runTask.bind(
+            null,
+            priorityLevel,
+            postTaskPriority,
+            controller,
+            callback
+          ),
+          options
+        )
+        .catch(handleAbortError);
+      return controller;
+    };
+    exports.unstable_shouldYield = function () {
+      return getCurrentTime() >= deadline;
+    };
+    exports.unstable_wrapCallback = function (callback) {
+      var parentPriorityLevel = currentPriorityLevel_DEPRECATED;
+      return function () {
+        var previousPriorityLevel = currentPriorityLevel_DEPRECATED;
+        currentPriorityLevel_DEPRECATED = parentPriorityLevel;
+        try {
+          return callback();
+        } finally {
+          currentPriorityLevel_DEPRECATED = previousPriorityLevel;
+        }
+      };
+    };
+  })();
