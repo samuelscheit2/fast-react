ccca3ce2eb6329f00ca6d4ba7e4d3e5c6ae34f99
samuelscheit <github@samuelscheit.com>
2026-05-10 07:48:26 +0200

    Add test-renderer mock scheduler shell

--- packages/react-test-renderer/index.js
+++ packages/react-test-renderer/index.js
@@ -5,6 +5,71 @@ const entrypoint = 'react-test-renderer';
 const placeholderVersion = '0.0.0-fast-react-test-renderer-placeholder';
 const unimplementedCode = 'FAST_REACT_UNIMPLEMENTED';
 const isProduction = process.env.NODE_ENV === 'production';
+const schedulerMockKeys = [
+  'log',
+  'reset',
+  'unstable_IdlePriority',
+  'unstable_ImmediatePriority',
+  'unstable_LowPriority',
+  'unstable_NormalPriority',
+  'unstable_Profiling',
+  'unstable_UserBlockingPriority',
+  'unstable_advanceTime',
+  'unstable_cancelCallback',
+  'unstable_clearLog',
+  'unstable_flushAll',
+  'unstable_flushAllWithoutAsserting',
+  'unstable_flushExpired',
+  'unstable_flushNumberOfYields',
+  'unstable_flushUntilNextPaint',
+  'unstable_forceFrameRate',
+  'unstable_getCurrentPriorityLevel',
+  'unstable_hasPendingWork',
+  'unstable_next',
+  'unstable_now',
+  'unstable_requestPaint',
+  'unstable_runWithPriority',
+  'unstable_scheduleCallback',
+  'unstable_setDisableYieldValue',
+  'unstable_shouldYield',
+  'unstable_wrapCallback'
+];
+
+const schedulerFunctionShapes = {
+  log: ['', 1],
+  reset: ['', 0],
+  unstable_advanceTime: ['', 1],
+  unstable_cancelCallback: ['', 1],
+  unstable_clearLog: ['', 0],
+  unstable_flushAll: ['', 0],
+  unstable_flushAllWithoutAsserting: [
+    'unstable_flushAllWithoutAsserting',
+    0
+  ],
+  unstable_flushExpired: ['', 0],
+  unstable_flushNumberOfYields: ['', 1],
+  unstable_flushUntilNextPaint: ['', 0],
+  unstable_forceFrameRate: ['', 0],
+  unstable_getCurrentPriorityLevel: ['', 0],
+  unstable_hasPendingWork: ['', 0],
+  unstable_next: ['', 1],
+  unstable_now: ['', 0],
+  unstable_requestPaint: ['', 0],
+  unstable_runWithPriority: ['', 2],
+  unstable_scheduleCallback: ['', 3],
+  unstable_setDisableYieldValue: ['', 1],
+  unstable_shouldYield: ['shouldYieldToHost', 0],
+  unstable_wrapCallback: ['', 1]
+};
+
+const schedulerConstantValues = {
+  unstable_IdlePriority: 5,
+  unstable_ImmediatePriority: 1,
+  unstable_LowPriority: 4,
+  unstable_NormalPriority: 3,
+  unstable_Profiling: null,
+  unstable_UserBlockingPriority: 2
+};
 
 function createUnsupportedError(exportName, action, detail) {
   const suffix = detail === undefined ? '' : ` ${detail}`;
@@ -48,6 +113,18 @@ function createUnsupportedFunction(exportName, length) {
   return defineFunctionShape(fn, exportName, length);
 }
 
+function createSchedulerUnsupportedFunction(exportName, name, length) {
+  const fn = function fastReactTestRendererSchedulerPlaceholder() {
+    throw createUnsupportedError(
+      `_Scheduler.${exportName}`,
+      'was called',
+      'The public Scheduler exposure is intentionally a throwing shape shell until react-test-renderer act and scheduling behavior are wired.'
+    );
+  };
+
+  return defineFunctionShape(fn, name, length);
+}
+
 function createRendererUnsupportedFunction(exportName, length, detail) {
   const fn = function fastReactTestRendererRendererPlaceholder() {
     throw createUnsupportedError(exportName, 'was called', detail);
@@ -57,47 +134,21 @@ function createRendererUnsupportedFunction(exportName, length, detail) {
 }
 
 function createSchedulerPlaceholder() {
-  const target = Object.create(null);
+  const scheduler = {};
 
-  Object.defineProperty(target, Symbol.toStringTag, {
-    configurable: true,
-    value: 'FastReactTestRendererUnimplementedScheduler'
-  });
-
-  return new Proxy(target, {
-    get(_target, property) {
-      if (property === Symbol.toStringTag) {
-        return 'FastReactTestRendererUnimplementedScheduler';
-      }
-
-      throw createUnsupportedError(
-        `_Scheduler.${String(property)}`,
-        'was accessed',
-        'The public Scheduler exposure is intentionally blocked until react-test-renderer act and scheduling behavior are wired.'
-      );
-    },
-    getOwnPropertyDescriptor() {
-      return undefined;
-    },
-    has(_target, property) {
-      throw createUnsupportedError(
-        `_Scheduler.${String(property)}`,
-        'was checked',
-        'The public Scheduler exposure is intentionally blocked until react-test-renderer act and scheduling behavior are wired.'
-      );
-    },
-    ownKeys() {
-      return [];
-    },
-    set(_target, property) {
-      throw createUnsupportedError(
-        `_Scheduler.${String(property)}`,
-        'was assigned',
-        'The public Scheduler exposure is intentionally blocked until react-test-renderer act and scheduling behavior are wired.'
-      );
+  for (const key of schedulerMockKeys) {
+    if (Object.hasOwn(schedulerConstantValues, key)) {
+      scheduler[key] = schedulerConstantValues[key];
+    } else {
+      const [name, length] = schedulerFunctionShapes[key];
+      scheduler[key] = createSchedulerUnsupportedFunction(key, name, length);
     }
-  });
+  }
+
+  return scheduler;
 }
+
+const schedulerPlaceholder = createSchedulerPlaceholder();
 
 function definePlaceholderMetadata(exportsObject) {
   Object.defineProperties(exportsObject, {
@@ -120,7 +171,7 @@ function definePlaceholderMetadata(exportsObject) {
 
 function createPlaceholderRenderer() {
   const renderer = {
-    _Scheduler: createSchedulerPlaceholder(),
+    _Scheduler: schedulerPlaceholder,
     root: undefined,
     toJSON: createRendererUnsupportedFunction(
       'create().toJSON',
@@ -173,7 +224,7 @@ function create() {
   return createPlaceholderRenderer();
 }
 
-exports._Scheduler = createSchedulerPlaceholder();
+exports._Scheduler = schedulerPlaceholder;
 exports.act = isProduction ? undefined : createUnsupportedFunction('act', 1);
 exports.create = defineFunctionShape(create, 'create', 2);
 exports.unstable_batchedUpdates = createUnsupportedFunction(
